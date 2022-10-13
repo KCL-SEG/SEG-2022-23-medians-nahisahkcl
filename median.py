fa2982ef9a83efb862dcc4935c15ef6c8e4ca173
Nahisah Nasleem <fathima.mohammad_nasleem@kcl.ac.uk>
2022-10-13 13:29:19 +0100

    start of task

--- median.py
+++ median.py
@@ -9,4 +9,14 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
+
+
+
+    len_numbers = len(numbers)
+    if len_numbers % 2 ==0:
+        tot = numbers[len_numbers/2] + numbers[(len_numbers/2)-1]
+        median = tot/2
+        return median
+    else:
+        median = numbers[(len_numbers-1)/2]
 print(numbers)
